bff4c3f92ec0f2702603d84c71bfc4a4e99f7e9b
ashutosh9897794237 <113291959+ashutosh9897794237@users.noreply.github.com>
2023-07-12 13:30:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,11 +2,12 @@ function fibonacci(num) {
 	while (num<20) {
 	if(num==0)
 		return 0;
-	if(num==1)
+	else if(num==1)
 		return 1;
-
+else{
 	return fibonacci(num) = fibonacci(num-1)+fibonacci(num-2);
 // your code here
+	}
 	}
 }
 
